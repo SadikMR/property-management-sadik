a364f101db7120af9e2e3ada204ae51b1014f70b
SadikMR <sadikmahmudraihan@gmail.com>
2026-06-19 16:24:14 +0600

    create model for location, propery,PropertyImage

--- property_app/admin.py
+++ property_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Location, Property, PropertyImage
 
-# Register your models here.
+admin.site.register(Location)
+admin.site.register(Property)
+admin.site.register(PropertyImage)
--- property_app/models.py
+++ property_app/models.py
@@ -1,3 +1,74 @@
-from django.db import models
+from django.contrib.gis.db import models
+from pgvector.django import VectorField
 
-# Create your models here.
+
+class Location(models.Model):
+    country = models.CharField(max_length=100)
+
+    city = models.CharField(max_length=100)
+
+    name = models.CharField(max_length=255)
+
+    name_embedding = VectorField(
+        dimensions=384,  
+        null=True,
+        blank=True
+    )
+
+    center = models.PointField(
+        geography=True,
+        srid=4326
+    )
+
+    def __str__(self):
+        return f"{self.name}, {self.city}"
+
+
+class Property(models.Model):
+    location = models.ForeignKey(
+        Location,
+        on_delete=models.CASCADE,
+        related_name="properties"
+    )
+
+    name = models.CharField(max_length=255)
+
+    description = models.TextField(
+        blank=True
+    )
+
+    amenities = models.TextField(
+        blank=True
+    )
+
+    center = models.PointField(
+        geography=True,
+        srid=4326
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class PropertyImage(models.Model):
+    property = models.ForeignKey(
+        Property,
+        on_delete=models.CASCADE,
+        related_name="images"
+    )
+
+    url = models.URLField(
+        blank=True
+    )
+
+    caption = models.CharField(
+        max_length=255,
+        blank=True
+    )
+
+    image = models.ImageField(
+        upload_to="property_images/"
+    )
+
+    def __str__(self):
+        return self.caption or f"Image {self.pk}"
