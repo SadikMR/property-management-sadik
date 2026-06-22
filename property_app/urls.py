5815e2c7f86558987872ae7c2f1f376003d64f0e
SadikMR <sadikmahmudraihan@gmail.com>
2026-06-22 12:52:38 +0600

    Updated structure for adopting the templates

---
+++ property_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.home, name = "home"),
+    path("search/", views.property_list, name = "property_list"),
+    path(
+        "property/<slug:slug>/",
+        views.property_detail,
+        name="property_detail",
+    )
+         
+]
--- property_app/views.py
+++ property_app/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.db.models import Q
 
-# Create your views here.
+from .models import (
+    Location,
+    Property,
+)
+
+def home(request):
+    return render(request, "home.html")
+
+def property_list(request):
+    query = request.GET.get(
+        "location",
+        ""
+    )
+
+    locations = Location.objects.filter(
+        Q(name__icontains=query)
+        |
+        Q(city__icontains=query)
+    )
+
+    properties = Property.objects.filter(
+        location__in=locations
+    )
+
+    return render(
+        request,
+        "property_list.html",
+        {
+            "properties": properties,
+            "query": query,
+        },
+    )
+
+def property_detail(request, slug):
+    return render(
+        request,
+        "property_detail.html",
+    )
